24efbd47e3befffccebba4f9975f1767b8b77679
wankhede04 <wankhedevijay04@gmail.com>
2020-10-17 20:59:51 +0530

    Added functionality to start the game.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  public startGame: boolean = false;
+
+  public startPlaying() {
+    this.startGame = true
+    document.getElementById('start-game').blur();
+  }
+}
