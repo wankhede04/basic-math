3c49c670d58fc52d5c41d2ab87993842686e3789
wankhede04 <wankhedevijay04@gmail.com>
2020-10-17 20:57:59 +0530

    Implemented dynamic functionality to get, set and calculate average solution time.

--- src/app/add-numbers/add-numbers.component.ts
+++ src/app/add-numbers/add-numbers.component.ts
@@ -1,16 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { IAdditionValues, ITimeStamps } from '../models/addition';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-numbers',
   templateUrl: './add-numbers.component.html',
   styleUrls: ['./add-numbers.component.scss']
 })
-export class AddNumbersComponent implements OnInit {
-  public averageSolutionTime: number;
+export class AddNumbersComponent implements OnDestroy {
+  public inputValue: number;
+  public isCorrectValue: boolean;
+  public averageSolutionTime: ITimeStamps;
+  public correctAnswers: number = 0;
 
-  constructor() { }
+  public calculationValues$: BehaviorSubject<IAdditionValues>;
+  public subscription: Subscription;
 
-  ngOnInit() {
-    this.averageSolutionTime = 1.2;
+  constructor() {
+    this.calculationValues$ = new BehaviorSubject({
+      firstDigit: this.generateAdditionValues(),
+      secondDigit: this.generateAdditionValues()
+    });
+
+    this.averageSolutionTime = {
+      start: Date.now(),
+      end: null,
+      average: null
+    };
+  }
+
+  public getInputValue(inputValue: number) {
+    this.subscription = this.calculationValues$.pipe(
+      take(1)
+    ).subscribe((calValues) => {
+      if (inputValue === calValues.firstDigit + calValues.secondDigit) {
+        this.averageSolutionTime.end = Date.now();
+
+        this.calculationValues$.next({
+          firstDigit: this.generateAdditionValues(),
+          secondDigit: this.generateAdditionValues()
+        });
+
+        this.inputValue = null;
+        this.isCorrectValue = true;
+        this.correctAnswers++;
+
+        this.getAverageTime();
+      } else {
+        this.isCorrectValue = false;
+      }
+    });
+  }
+
+  public generateAdditionValues(): number {
+    return Math.floor(Math.random() * 10)
+  }
+
+  public getAverageTime() {
+    this.averageSolutionTime.average = (this.averageSolutionTime.end - this.averageSolutionTime.start) / this.correctAnswers;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
