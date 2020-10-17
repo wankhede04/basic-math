c320e3d40d5c58277647128ae7a20293d515172e
wankhede04 <wankhedevijay04@gmail.com>
2020-10-17 20:58:54 +0530

    Added forms module to use ng-model.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AddNumbersComponent } from './add-numbers/add-numbers.component';
 import { SolutionTimeComponent } from './solution-time/solution-time.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SolutionTimeComponent } from './solution-time/solution-time.component';
     SolutionTimeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
